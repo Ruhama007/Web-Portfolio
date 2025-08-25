eb975dd603782b93abd51ecc772814a503f50724
Nuisrat <nuisrat.jannat007@gmail.com>
2025-08-25 22:33:53 +0600

    Send Email

---
+++ send_email.py
@@ -0,0 +1,18 @@
+import smtplib, ssl
+
+host = "smtp.gmail.com"
+port = 465
+
+username = "nuisratjannat09@gmail.com"
+password = "ypck edsw fxtr jskm"
+
+receiver = "nuisratjannat09@gmail.com"
+context = ssl.create_default_context()
+
+message = """\
+Subject: Hi!
+Hi! How are you?
+"""
+with smtplib.SMTP_SSL(host, port, context=context) as server:
+    server.login(username, password)
+    server.sendmail(username, receiver, message)
